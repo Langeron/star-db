ded9c588997a74d1b3d4c58b99d0c160c47adb4a
dgoncharenko <dgoncharenko@crispwp.com>
2020-03-22 14:29:34 +0200

    propTypes and defaultProps

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './item-list.css';
 
 const ItemList = (props) => {
   const { data, onItemSelected, children: renderLabel } = props;
+
   const items = data.map((item) => {
     const { id } = item;
     const label = renderLabel(item);
+
     return (
       <li className="list-group-item"
         key={id}
@@ -15,11 +18,23 @@ const ItemList = (props) => {
       </li>
     )
   });
+
   return (
     <ul className="item-list list-group">
       {items}
     </ul>
   );
+
+};
+
+ItemList.defaultProps = {
+  onItemSelected: () => {}
+};
+
+ItemList.propTypes = {
+  onItemSelected: PropTypes.func, 
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  children: PropTypes.func.isRequired
 }
 
 export default ItemList;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import SwapiService from '../../services/swapi-service';
 
@@ -7,6 +8,14 @@ import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
 
 export default class RandomPlanet extends Component {
+
+  static defaultProps = {
+    updateInterval: 10000
+  };
+
+  static propTypes = {
+    updateInterval: PropTypes.number
+  }
 
   swapiService = new SwapiService();
 
@@ -17,8 +26,9 @@ export default class RandomPlanet extends Component {
   }
 
   componentDidMount() {
+    const { updateInterval } = this.props;
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 3000);
+    this.interval = setInterval(this.updatePlanet, updateInterval);
   }
 
   componentWillUnmount() {
@@ -62,11 +72,12 @@ export default class RandomPlanet extends Component {
         {content}
       </div>
     );
-  }
-}
+  };
+};
 
-const PlanetView = ({planet}) => {
-  const {  id, population, rotationPeriod, diameter, name } = planet;
+
+const PlanetView = ({ planet }) => {
+  const { id, population, rotationPeriod, diameter, name } = planet;
   return (
     <React.Fragment>
       <img className="planet-image" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet" />
